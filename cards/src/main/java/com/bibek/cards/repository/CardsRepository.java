c4639d9a545914b777e9a824b16fa3b369a728f9
Bibek Regmi <vivekregmi83@gmail.com>
2025-07-21 21:07:07 +0545

    cards service added

---
+++ cards/src/main/java/com/bibek/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.bibek.cards.repository;
+
+import com.bibek.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
