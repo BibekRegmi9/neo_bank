dab6f8f2382eec75038d35797227c50c6ab9fab0
Bibek Regmi <vivekregmi83@gmail.com>
2025-09-03 23:01:14 +0545

    added env

--- accounts/src/main/java/com/bibek/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/bibek/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.bibek.accounts.controller;
 
 import com.bibek.accounts.constants.AccountsConstants;
+import com.bibek.accounts.dto.AccountsContactInfoDto;
 import com.bibek.accounts.dto.CustomerDto;
 import com.bibek.accounts.dto.ErrorResponseDto;
 import com.bibek.accounts.dto.ResponseDto;
@@ -14,6 +15,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,10 +28,21 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Accounts", description = "Accounts APIs")
 @RestController
 @RequestMapping(path = "/api/v1/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Validated
 public class AccountsController {
     private IAccountsService accountsService;
+
+    // using value annotation
+    @Value("${build.version}")
+    private String buildVersion;
+
+    // using environment
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDto accountsContactInfoDto;
 
     @Operation(
             summary = "Creates a new account for the given customer",
@@ -98,5 +114,23 @@ public class AccountsController {
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
         }
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity getBuildVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @GetMapping("/java-version")
+    public ResponseEntity getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @GetMapping("/contact-info")
+    public ResponseEntity getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK).body(accountsContactInfoDto);
     }
 }
